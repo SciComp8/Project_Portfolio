5f81cb608bcca478c8cef8eef9951008e8738d30
Angela <90419301+lancome1@users.noreply.github.com>
2023-02-24 09:19:21 -0500

    Create Create_OverlapPlot.R

---
+++ BMARNASeq/Create_OverlapPlot.R
@@ -0,0 +1,20 @@
+load("../ApplicationData/derived/overlapplot_image.RData")
+
+suppressPackageStartupMessages(easypackages::libraries("microbenchmark", "bannerCommenter", "fst", "parallel", "ggplot2", "data.table"))
+
+date.analysis <- format(Sys.Date(), "%Y%b%d")
+var.vec <- c("BMI", "AGE", "SEX", "WBC", "BMIxSEX")
+
+for (var.i in var.vec) {
+g <- boxplot.data.new[variable==var.i] |>
+  ggplot(aes(y = num.cDEGs, x = method, group = method, fill = method)) + 
+  geom_boxplot() + 
+  stat_summary(fun = median, geom = "line", aes(group = 1), color = "#0073C2FF") +  
+  facet_wrap(vars(threshold)) + 
+  labs(y = "Number of common differentially expressed genes", x = "Method") + 
+  theme_bw() + 
+  theme(legend.position = "none",
+        axis.text.x = element_text(size = 9, angle = 90, hjust = 1, color = "black", vjust = 0.5))
+  ggsave(filename = sprintf("../ApplicationResult/Multi_Interaction/RandomSeed/cDEGs/Boxplot/%s_%s.png", date.analysis, var.i),
+         plot = g, device = "png", width = 9, height = 6, units = "in")
+}
