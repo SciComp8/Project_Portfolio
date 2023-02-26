bf645b2fdc9299a0b06beeee430a7fdd3816adc2
Angela <90419301+lancome1@users.noreply.github.com>
2023-02-25 19:13:34 -0500

    Update Create_cDEGsDuplicateRate.R

--- BMARNASeq/Create_cDEGsDuplicateRate.R
+++ BMARNASeq/Create_cDEGsDuplicateRate.R
@@ -5,14 +5,34 @@ seed.vec <- c(8809678, 98907, 233, 556, 7890, 120, 2390, 778, 666, 99999)
 duplicated_rate_cDEGs <- function(var.name = "BMI", method.name = "BMAseq") {
   for (threshold.i in threshold.vec) {
     for (seed.i in seed.vec) {
-      load(sprintf("../ApplicationData/derived/RandomSeed/Top5000/%sMultiInt%s.RData", method.name, seed.i))
-      cDEGs <- c(cDEGs, intersect(names(get(sprintf("%s.eFDR.Main.train", method.name))[[var.name]][1:threshold.i]), 
-                                  names(get(sprintf("%s.eFDR.Main.test", method.name))[[var.name]][1:threshold.i])))
+      if (method.name == "BMAseq" & var.name != "BMIxSEX") {
+        load(sprintf("../ApplicationData/derived/RandomSeed/Top5000/%sMultiInt%s.RData", method.name, seed.i))
+        cDEGs <- c(cDEGs, intersect(names(get(sprintf("%s.eFDR.Main.train", method.name))[[var.name]][1:threshold.i]), 
+                                    names(get(sprintf("%s.eFDR.Main.test", method.name))[[var.name]][1:threshold.i])))
+        } else if (method.name == "BMAseq" & var.name == "BMIxSEX") {
+        load(sprintf("../ApplicationData/derived/RandomSeed/Top5000/%sMultiInt%s.RData", method.name, seed.i))
+        cDEGs <- c(cDEGs, intersect(names(get(sprintf("%s.eFDR.Interaction.train", method.name))[[var.name]][1:threshold.i]), 
+                                    names(get(sprintf("%s.eFDR.Interaction.test", method.name))[[var.name]][1:threshold.i])))
+      } else if (method.name %in% c("DESeq", "edgeR")) {
+        load(sprintf("../ApplicationData/derived/RandomSeed/Top5000/%sMultiInt%s.RData", method.name, seed.i))
+        cDEGs <- c(cDEGs, intersect(names(get(sprintf("%s.eFDR.GeneName.train", method.name))[[var.name]][1:threshold.i]), 
+                                    names(get(sprintf("%s.eFDR.GeneName.test", method.name))[[var.name]][1:threshold.i])))
+      } else if (method.name == "VoomLimma") {
+        load(sprintf("../ApplicationData/derived/RandomSeed/Top5000/%sMultiInt%s.RData", method.name, seed.i))
+        method.name2 <- "voom"
+        cDEGs <- c(cDEGs, intersect(names(get(sprintf("%s.eFDR.train2", method.name2))[[var.name]][1:threshold.i]), 
+                                    names(get(sprintf("%s.eFDR.test2", method.name2))[[var.name]][1:threshold.i])))
+      } else {
+        load(sprintf("../ApplicationData/derived/RandomSeed/Top5000/%sMultiInt%s.RData", method.name, seed.i))
+        cDEGs <- c(cDEGs, intersect(names(get(sprintf("%s.eFDR.train2", method.name))[[var.name]][1:threshold.i]), 
+                                    names(get(sprintf("%s.eFDR.test2", method.name))[[var.name]][1:threshold.i])))
+      }
     }
     print(paste("The duplicated rate of cDEGs associated with", var.name, "identified by", method.name, "among 10 random seeds for the threshold", threshold.i, "is", sum(duplicated(cDEGs))/length(cDEGs)))
     cDEGs <- NULL
   }
 }
+
 
 mapply(duplicated_rate_cDEGs,
        c("BMI","AGE","SEX","WBC","BMIxSEX"), 
