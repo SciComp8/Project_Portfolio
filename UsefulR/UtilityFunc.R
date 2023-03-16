a6b427ca54a047fac69b8a4a5b83cb97d093fc0a
Anni <90419301+anniliu9@users.noreply.github.com>
2023-03-16 17:59:14 -0400

    Update UtilityFunc.R

--- UsefulR/UtilityFunc.R
+++ UsefulR/UtilityFunc.R
@@ -5,5 +5,5 @@ unlink("~/Downloads/Lab7_GWAS/lab7_cache", recursive = T)
 read.delim(file, sep = ",")
 
 
-# Stack two columns into one long vector
+# Stack two columns into one long vector == translocate the second column into the bottom of the first column
 mapply(c, geno_import[, seq(1, ncol(geno_import), 2)], geno_import[, seq(2, ncol(geno_import), 2)])
