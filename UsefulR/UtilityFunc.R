6531ee1185478cd100f6a561f230ca26fdae5a80
Anni <90419301+anniliu9@users.noreply.github.com>
2023-03-16 18:28:34 -0400

    Update UtilityFunc.R

--- UsefulR/UtilityFunc.R
+++ UsefulR/UtilityFunc.R
@@ -12,3 +12,6 @@ args(mapply)
 
 # Calculate the Moore-Penrose generalized inverse of a matrix X
 ginv(t(x) %*% x)
+
+# Calcuate the distribution function for the F distribution with df1 and df2 degrees of freedom
+pf(Fstatistic, df_M, df_E, lower.tail = F)
