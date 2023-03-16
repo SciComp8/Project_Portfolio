46028b7fa6f3331725366d03f9608b6f7e9d283a
Anni <90419301+anniliu9@users.noreply.github.com>
2023-03-16 17:54:07 -0400

    Update and rename WrangleFileDir.R to UtilityFunc.R

---
+++ UsefulR/UtilityFunc.R
@@ -0,0 +1,9 @@
+# Deletes the file(s) or directories 
+unlink("~/Downloads/Lab7_GWAS/lab7_cache", recursive = T)
+
+# Read the file
+read.delim(file, sep = ",")
+
+
+# Stack two columns into one long vector
+mapply(c, geno_import[, seq(1, ncol(geno_import), 2)], geno_import[, seq(2, ncol(geno_import), 2)])
--- UsefulR/WrangleFileDir.R
+++
@@ -1,5 +0,0 @@
-# Deletes the file(s) or directories 
-unlink("~/Downloads/Lab7_GWAS/lab7_cache", recursive = T)
-
-# Read the file
-read.delim(file, sep = ",")
