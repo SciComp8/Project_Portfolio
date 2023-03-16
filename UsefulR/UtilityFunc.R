380bdd14b3ca2965ee2e1277aae01700978a6221
Anni <90419301+anniliu9@users.noreply.github.com>
2023-03-16 18:29:40 -0400

    Update UtilityFunc.R

--- UsefulR/UtilityFunc.R
+++ UsefulR/UtilityFunc.R
@@ -9,9 +9,3 @@ mapply(c, geno_import[, seq(1, ncol(geno_import), 2)], geno_import[, seq(2, ncol
 
 # Check the arguments of a function
 args(mapply)
-
-# Calculate the Moore-Penrose generalized inverse of a matrix X
-ginv(t(x) %*% x)
-
-# Calcuate the distribution function for the F distribution with df1 and df2 degrees of freedom
-pf(Fstatistic, df_M, df_E, lower.tail = F)
