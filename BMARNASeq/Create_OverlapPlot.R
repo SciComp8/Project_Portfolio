8ce97aa1350b084daabc839f625988e1551a5f54
Anni <90419301+anniliu9@users.noreply.github.com>
2023-03-22 16:20:58 -0400

    Update Create_OverlapPlot.R

--- BMARNASeq/Create_OverlapPlot.R
+++ BMARNASeq/Create_OverlapPlot.R
@@ -7,17 +7,17 @@ date.analysis <- format(Sys.Date(), "%Y%b%d")
 var.vec <- c("BMI", "AGE", "SEX", "WBC", "BMIxSEX")
 
 for (var.i in var.vec) {
-g <- boxplot.data.new[variable==var.i] |>
-  ggplot(aes(y = num.cDEGs, x = method, group = method, fill = method)) + 
-  geom_boxplot() + 
-  stat_summary(fun = median, geom = "line", aes(group = 1), color = "#0073C2FF") +  
-  facet_wrap(vars(threshold)) + 
-  labs(y = "Number of common differentially expressed genes", x = "Method") + 
-  theme_bw() + 
-  theme(legend.position = "none",
-        axis.text.x = element_text(size = 9, angle = 90, hjust = 1, color = "black", vjust = 0.5))
-  ggsave(filename = sprintf("../ApplicationResult/Multi_Interaction/RandomSeed/numcDEGs/Boxplot/%s_%s.png", date.analysis, var.i),
-         plot = g, device = "png", width = 9, height = 6, units = "in")
+  g <- boxplot.data.new[variable==var.i] |>
+    ggplot(aes(y = num.cDEGs, x = method, group = method, fill = method)) + 
+    geom_boxplot() + 
+    stat_summary(fun = median, geom = "line", aes(group = 1), color = "#0073C2FF") +  
+    facet_wrap(vars(threshold)) + 
+    labs(y = "Number of common differentially expressed genes", x = "Method") + 
+    theme_bw() + 
+    theme(legend.position = "none",
+          axis.text.x = element_text(size = 9, angle = 90, hjust = 1, color = "black", vjust = 0.5))
+    ggsave(filename = sprintf("../ApplicationResult/Multi_Interaction/RandomSeed/numcDEGs/Boxplot/%s_%s.png", date.analysis, var.i),
+           plot = g, device = "png", width = 9, height = 6, units = "in")
 }
 
 ## Batch plotting and saving - rate.cDEGs
@@ -25,15 +25,15 @@ date.analysis <- format(Sys.Date(), "%Y%b%d")
 var.vec <- c("BMI", "AGE", "SEX", "WBC", "BMIxSEX")
 
 for (var.i in var.vec) {
-g <- boxplot.data.new2[variable==var.i] |>
-  ggplot(aes(y = rate.cDEGs, x = method, group = method, fill = method)) + 
-  geom_boxplot() + 
-  stat_summary(fun = median, geom = "line", aes(group = 1), color = "#0073C2FF") +  
-  facet_wrap(vars(threshold)) + 
-  labs(y = "Rate of common differentially expressed genes", x = "Method") + 
-  theme_bw() + 
-  theme(legend.position = "none",
-        axis.text.x = element_text(size = 9, angle = 90, hjust = 1, color = "black", vjust = 0.5))
-  ggsave(filename = sprintf("../ApplicationResult/Multi_Interaction/RandomSeed/ratecDEGs/Boxplot/%s_%s.png", date.analysis, var.i),
-         plot = g, device = "png", width = 9, height = 6, units = "in")
+  g <- boxplot.data.new2[variable==var.i] |>
+    ggplot(aes(y = rate.cDEGs, x = method, group = method, fill = method)) + 
+    geom_boxplot() + 
+    stat_summary(fun = median, geom = "line", aes(group = 1), color = "#0073C2FF") +  
+    facet_wrap(vars(threshold)) + 
+    labs(y = "Rate of common differentially expressed genes", x = "Method") + 
+    theme_bw() + 
+    theme(legend.position = "none",
+          axis.text.x = element_text(size = 9, angle = 90, hjust = 1, color = "black", vjust = 0.5))
+    ggsave(filename = sprintf("../ApplicationResult/Multi_Interaction/RandomSeed/ratecDEGs/Boxplot/%s_%s.png", date.analysis, var.i),
+           plot = g, device = "png", width = 9, height = 6, units = "in")
 }
