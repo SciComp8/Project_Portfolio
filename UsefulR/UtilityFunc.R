5884c5dc9e2f22a63b2f7ac0ef938a2ceb514635
Anni <90419301+anniliu9@users.noreply.github.com>
2023-03-30 15:30:30 -0400

    Update UtilityFunc.R

--- UsefulR/UtilityFunc.R
+++ UsefulR/UtilityFunc.R
@@ -4,6 +4,16 @@ unlink("~/Downloads/Lab7_GWAS/lab7_cache", recursive = T)
 # Read the file
 read.delim(file, sep = ",")
 
+# Write the file
+file.name = "Matrix.txt"
+write(sprintf("probability matrix: x= %s", ncol(object1)),
+      file.name, append = T)
+write.table(object2,
+            file.name, sep = "\t",
+            quote = F, 
+            col.names = F,
+            append = T)
+
 # Stack two columns into one long vector == translocate the second column into the bottom of the first column
 mapply(c, geno_import[, seq(1, ncol(geno_import), 2)], geno_import[, seq(2, ncol(geno_import), 2)])
 
