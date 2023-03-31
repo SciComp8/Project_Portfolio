b5ec40293d63bacee10cfaba527e7274c8f5b883
Anni <90419301+anniliu9@users.noreply.github.com>
2023-03-30 21:02:22 -0400

    Update MathFunc.R

--- UsefulR/MathFunc.R
+++ UsefulR/MathFunc.R
@@ -3,3 +3,7 @@ ginv(t(x) %*% x)
 
 # Calcuate the distribution function for the F distribution with df1 and df2 degrees of freedom
 pf(Fstatistic, df_M, df_E, lower.tail = F)
+
+# Fit the generalized models
+mdl.fit <- pscl::zeroinfl(count~1|1, link="logit", dist="negbin",data=test.dat)
+mdl.fit <- VGAM::vglm(count~1, negbinomial, data=test.dat)
