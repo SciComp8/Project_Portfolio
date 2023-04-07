3c00b6a0ed0ba10256ff0d2e364bc2aa6e56ff3a
Anni <90419301+anniliu9@users.noreply.github.com>
2023-04-07 12:26:15 -0400

    Update Filename.sh

--- UsefulBash/Filename.sh
+++ UsefulBash/Filename.sh
@@ -1,3 +1,5 @@
-# Extract the base filenames (without the .fastq.gz extension) of the input files specified by the first and second arguments 
-r1=$(basename $1 .fastq.gz)
-r2=$(basename $2 .fastq.gz)
+# Extract the base filenames (without the .tsv.gz extension) of the input files specified by the first and second arguments 
+r1=$(basename $1 .tsv.gz)
+r2=$(basename $2 .tsv.gz)
+echo $r1 $r2
+# bash Filename.sh barcodes1.tsv.gz barcodes2.tsv.gz
