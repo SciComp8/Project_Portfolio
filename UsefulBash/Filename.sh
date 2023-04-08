3c2267f3393c5c989307835431a9845237b08f26
Anni <90419301+anniliu9@users.noreply.github.com>
2023-04-07 21:48:51 -0400

    Update Filename.sh

--- UsefulBash/Filename.sh
+++ UsefulBash/Filename.sh
@@ -3,3 +3,10 @@ r1=$(basename $1 .tsv.gz)
 r2=$(basename $2 .tsv.gz)
 echo $r1 $r2
 # bash Filename.sh barcodes1.tsv.gz barcodes2.tsv.gz
+
+
+# Remove files with specific shared base filenames
+for i in `ls | grep .fastq`
+do
+basename=$(echo $i| sed 's/\50K_2_bowtie2//g')
+rm $basename.sam
