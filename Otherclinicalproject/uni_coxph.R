7e9f1155b3135381e4f6cc2424cf44c1cdc2168e
Anni <90419301+anniliu9@users.noreply.github.com>
2023-04-18 19:52:48 -0400

    Update uni_coxph.R

--- Otherclinicalproject/uni_coxph.R
+++ Otherclinicalproject/uni_coxph.R
@@ -3,8 +3,7 @@
 uni_coxph <- function(surv.time, surv.event) {
   out <- mclapply(1:length(vars.ana),
                   function(i) {
-                    vars.missing <- ifelse(is.na(dat.work[, vars.ana[i]]), 
-                                         "missing", "non.missing") %>%
+                    vars.missing <- ifelse(is.na(dat.work[, vars.ana[i]]), "missing", "non.missing") |>
                       factor(levels = c("non.missing", "missing"))
                     res.i <- try(coxph(formula("Surv(" %0% surv.time %0% "," %0% surv.event %0% ") ~ " %0% vars.ana[i]), data = dat.work), silent = T) # Store errors due to insufficient levels of a factor variable
                     if(any(vars.missing == "missing")) {
