b001725df923fc37e548f18b94cd9a301f0f592e
Anni <90419301+anniliu7@users.noreply.github.com>
2023-05-28 22:15:02 -0400

    Create ManuscriptTheme.R

---
+++ UsefulR/ManuscriptTheme.R
@@ -0,0 +1,13 @@
+theme_BMA <- function(
+    base_size = 12,
+    base_family = "Arial"
+){
+  theme_classic(base_size = base_size, base_family = base_family)+
+    theme(
+      legend.position = "none",
+      panel.background = element_rect(fill = "white", colour = "white"),
+      axis.text.x = element_text(size = 9, angle = 90, hjust = 1, color = "black", vjust = 0.5),
+      strip.background = element_rect(fill = "white", color = "black", linewidth = 0.6), # Customize the box border in strip
+      strip.placement = "inside"
+    )
+}
