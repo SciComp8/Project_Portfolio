e0bac4f7b24499243c98ddbf94f70a484708875d
Anni <90419301+anniliu7@users.noreply.github.com>
2023-05-29 21:34:33 -0400

    Update Format_glmVarName.R

--- UsefulR/Format_glmVarName.R
+++ UsefulR/Format_glmVarName.R
@@ -7,25 +7,27 @@ alter.glm.rownames <- function(model) {
     } else if (i == "Age.Dx") {
       var.names[v] <- "Age at Diagnosis"
     } else if (grepl("*.[a-z][A-Z]", i)) {
-      if (grepl("^Race", i)) {
+      if (grepl("^Race.A", i)) {
         var.names[v] <- paste0(substring(i, regexpr("*.[a-z][A-Z]", i) + 2), " (vs Non-Asian)")
-      } else {
+      } else if (grepl("^RaceE", i)) {
+        var.names[v] <- paste0(substring(i, regexpr("*.[0-9][A-Z]", i) + 2), " (vs Non-Hispanic White)")
+      } else if (grepl("^Ins", i)) {
         var.names[v] <- paste0(substring(i, regexpr("*.[a-z][A-Z]", i) + 2), " (vs Private)")
       }
     } else if (i == "MMGSDNo") {
-        var.names[v] <- "Mammo Screen-Detected No (vs Mammo Screen-Detected Yes)"
+      var.names[v] <- "Mammo Screen-Detected No (vs Mammo Screen-Detected Yes)"
     } else if (i == "MMGOYes") {
-        var.names[v] <- "Mammo-Occult Yes (vs Mammo-Occult No)"
+      var.names[v] <- "Mammo-Occult Yes (vs Mammo-Occult No)"
     } else if (i == "MRISDYes") {
-        var.names[v] <- "MRI Screen-Detected Yes (vs MRI Screen-Detected No)"
-    } else if (grepl("*.2[A-Z]", i)) {
+      var.names[v] <- "MRI Screen-Detected Yes (vs MRI Screen-Detected No)"
+    } else if (grepl("*.[0-9][A-Z]", i)) {
         if (grepl("^Hos", i)) {
-            var.names[v] <- "NYPQ (vs WCM)"
+          var.names[v] <- "NYPQ (vs WCM)"
         } else {
-          var.names[v] <- paste0(substring(i, regexpr("*.2[A-Z]", i) + 2), " (vs Heterogenously dense)")
+          var.names[v] <- paste0(substring(i, regexpr("*.[0-9][A-Z]", i) + 2), " (vs Heterogenously dense)")
         }
     } else if (i == "ERNegative") {
-        var.names[v] <- "ER Negative (vs ER Positive)"
+      var.names[v] <- "ER Negative (vs ER Positive)"
     }
   }
   return(var.names)
