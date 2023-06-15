de9edde2ca8830abfcdfbb20f3f400a256d9643e
Anni <90419301+anniliu7@users.noreply.github.com>
2023-06-15 13:25:37 -0400

    Update Create_Pairwise_cDEGsVennDiagram_v1.R

--- BMARNASeq/Create_Pairwise_cDEGsVennDiagram_v1.R
+++ BMARNASeq/Create_Pairwise_cDEGsVennDiagram_v1.R
@@ -14,7 +14,7 @@ get_venndata <- function(seed.i = 8809678,
                          model.method.2 = "DESeq2",
                          threshold = 5000,
                          ...){
-  file.name.1 <- sprintf("../ApplicationData/derived/RandomSeed/Top5000/%sModel/%s%s%s.RData", 
+  file.name.1 <- sprintf("../ApplicationData/derived/RandomSeed/Top5000/%sModel/Trim/%s%s%s.RData", 
                          model.type.1, model.method.1, model.type.1, seed.i)
   load(file.name.1)
   if (model.method.1 == "BMAseq") {
@@ -28,13 +28,13 @@ get_venndata <- function(seed.i = 8809678,
     cDEGs.1 <- intersect(edgeR.eFDR.GeneName.train[[var.name]][1:threshold],
                          edgeR.eFDR.GeneName.test[[var.name]][1:threshold])
   } else if (model.method.1 == "eBayes") {
-    cDEGs.1 <- intersect(names(edgeR.eFDR.GeneName.train[[var.name]][1:threshold]),
-                         names(edgeR.eFDR.GeneName.test[[var.name]][1:threshold]))
+    cDEGs.1 <- intersect(names(eBayes.eFDR.train2[[var.name]][1:threshold]),
+                         names(eBayes.eFDR.test2[[var.name]][1:threshold]))
   } else if (model.method.1 == "VoomLimma") {
     cDEGs.1 <- intersect(names(voom.eFDR.train2[[var.name]][1:threshold]),
-                         names(voom.eFDR.train2[[var.name]][1:threshold]))
+                         names(voom.eFDR.test2[[var.name]][1:threshold]))
   }
-  file.name.2 <- sprintf("../ApplicationData/derived/RandomSeed/Top5000/%sModel/%s%s%s.RData", 
+  file.name.2 <- sprintf("../ApplicationData/derived/RandomSeed/Top5000/%sModel/Trim/%s%s%s.RData", 
                          model.type.2, model.method.2, model.type.2, seed.i)
   load(file.name.2)
   if (model.method.2 == "DESeq2") {
@@ -45,13 +45,14 @@ get_venndata <- function(seed.i = 8809678,
                          edgeR.eFDR.GeneName.test[[var.name]][1:threshold])
   } else if (model.method.2 == "eBayes") {
     cDEGs.2 <- intersect(names(eBayes.eFDR.train2[[var.name]][1:threshold]),
-                         names(eBayes.eFDR.train2[[var.name]][1:threshold]))
+                         names(eBayes.eFDR.test2[[var.name]][1:threshold]))
   } else if (model.method.2 == "VoomLimma") {
     cDEGs.2 <- intersect(names(voom.eFDR.train2[[var.name]][1:threshold]),
-                         names(voom.eFDR.train2[[var.name]][1:threshold]))
+                         names(voom.eFDR.test2[[var.name]][1:threshold]))
   }
   return(list(cDEGs.1, cDEGs.2))
 }
+
 
 plot_venn <- function(seed.i = 8809678, 
                       var.name = "BMI", 
@@ -61,9 +62,7 @@ plot_venn <- function(seed.i = 8809678,
                       model.type.2 = "Uni",
                       threshold = 5000,
                       ...) {
-  
-  usr.args <- match.call()[-1] # Extract argument values used in an R function call
-  
+
   pair_venndata <- get_venndata(seed.i = seed.i, 
                                 var.name = var.name, 
                                 model.method.1 = model.method.1,
@@ -75,20 +74,20 @@ plot_venn <- function(seed.i = 8809678,
   # print(pair_venndata)
   
   label_names <- c(paste0(ifelse(
-    usr.args[["model.method.1"]] == "BMAseq",
+    model.method.1 == "BMAseq",
     "BMAseq",
     ifelse(
-      usr.args[["model.type.1"]] == "Multi",
-      paste0(usr.args[["model.method.1"]], "_MVM"),
-      paste0(usr.args[["model.method.1"]], "_UVM")
+      model.type.1 == "Multi",
+      paste0(model.method.1, "_MVM"),
+      paste0(model.method.1, "_UVM")
     )
   )),
   paste0(ifelse(
-    usr.args[["model.type.2"]] == "Multi",
-    paste0(usr.args[["model.method.2"]], "_MVM"),
-    paste0(usr.args[["model.method.2"]], "_UVM")
+    model.type.2 == "Multi",
+    paste0(model.method.2, "_MVM"),
+    paste0(model.method.2, "_UVM")
   )))
-    
+  
   venn_data <- list(pair_venndata[[1]], pair_venndata[[2]])
   names(venn_data) <- label_names
   
@@ -104,7 +103,7 @@ plot_venn <- function(seed.i = 8809678,
     scale_fill_manual(values = c("#3B499299", "#BB002199", "#00828099")) +
     theme(legend.position = "none",
           plot.title = element_text(hjust = 0.5, colour = "red")) + 
-  ggtitle(usr.args[["var.name"]])
+  ggtitle(var.name)
   return(make_venn)
 }
 
@@ -135,5 +134,5 @@ p3 <- plot_venn(seed.i = 8809678,
 
 plot_panel <- grid.arrange(p1, p2, p3, ncol = 3)
 date.analysis <- format(Sys.Date(), "%Y%b%d")
-file.name <- sprintf("../ApplicationResult/AddViz/VennDigram/Top5000/%s_%s.png", date.analysis, "BMI") # EPS does not support semi-transparency; tiff produces the MB-size file
+file.name <- sprintf("../ApplicationResult/AddViz/VennDigram/Top5000/%s_%s.png", date.analysis, "BMI") # EPS does not support semi-transparency; tiff produces MB-size file
 ggsave(filename = file.name, plot = plot_panel, device = "png", dpi = 600, width = 9, height = 6, units = "in")
