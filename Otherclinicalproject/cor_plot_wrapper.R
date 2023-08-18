a5a6eece22015b8a5f2fa67a1d450f6f28d6cbfe
Anni <90419301+anniliu7@users.noreply.github.com>
2023-08-18 10:12:00 -0400

    Update cor_omnipotent.R

--- Otherclinicalproject/cor_plot_wrapper.R
+++ Otherclinicalproject/cor_plot_wrapper.R
@@ -1,18 +1,24 @@
-easypackages::libraries("labelled", "BTKR", "corrr", "multcomp") |> suppressPackageStartupMessages()
+easypackages::libraries("tidyverse", "readxl", "gtsummary", "flextable", "stringi", "fst", "broom", "labelled", "data.table", "BTKR", "corrr", "multcomp", "survival", "GGally") |> suppressPackageStartupMessages() |> suppressPackageStartupMessages()
 
-cor_plot_wrapper <-
-  function(cor_method = "spearman",
-           var_y = "BMI.at.diagnosis",
-           y_label = "Body mass index",
-           var_focus = NULL,
-           data_set = NULL) {
-    data_set <- data_set
-    yname <- var_y
-    ylab <- y_label
-    y <- data_set[, yname]
-    vars.ana <- var_focus
-    vars.ana.class <- unlist(lapply(data_set[, vars.ana], function(x)
-      paste(class(x), collapse = ".")))
+cor_omnipotent <- function(var_y = "BMI",
+                           y_label = "Body mass index",
+                           data_set = data_set,
+                           var_focus = c("BMI",
+                                         "Height",
+                                         "Weight",
+                                         "Tumor.size.cm",
+                                         "Oncotype.score",
+                                         "Age.at.diagnosis"),
+                           cor_method = "spearman",
+                           cor_format = c("pairwise_cor_plot", "cor_table", "ggpairs")) {
+  cor_format <-
+    match.arg(cor_format, c("pairwise_cor_plot", "cor_table", "ggpairs"))
+  if (cor_format == "pairwise_cor_plot") {
+    y <- data_set[, var_y]
+    vars.ana <- var_focus[-which(var_y %in% var_focus)]
+    vars.ana.class <-
+      unlist(lapply(data_set[, vars.ana], function(x)
+        paste(class(x), collapse = ".")))
     vars.ana.ch <- vars.ana[grep("factor", vars.ana.class)]
     
     vars.ana.cat <- rep(0, length(vars.ana))
@@ -22,7 +28,7 @@ cor_plot_wrapper <-
     names(out) <- vars.ana
     
     for (i in 1:length(vars.ana)) {
-      cat(paste0("\n#### ", yname, " ~ ", vars.ana[i], "\n"))
+      cat(paste0("\n#### ", var_y, " ~ ", vars.ana[i], "\n"))
       x <- data_set[, vars.ana[i]]
       if (vars.ana.cat[i] == 1) {
         xlevs <- levels(factor(x))
@@ -52,7 +58,7 @@ cor_plot_wrapper <-
           y = y,
           x = factor(as.vector(x), levels = xlevs),
           type = "bxp",
-          y.plab = ylab,
+          y.plab = y_label,
           x.plab = label_attribute(data_set[[vars.ana[i]]]),
           xnames = xlevs,
           mar = c(3.5, 3.5, 0.5, 0.5),
@@ -60,10 +66,12 @@ cor_plot_wrapper <-
           cex.lab = 1,
           cex.axis = 1
         )
-        legend("topleft",
-               legend = paste0(compare_method, ":", p.txt),
-               bty = "n",
-               cex = 1)
+        legend(
+          "topleft",
+          legend = paste0(compare_method, ":", p.txt),
+          bty = "n",
+          cex = 1
+        )
         cat("\n")
       } else {
         out[[i]] <- cor.test(y, x, method = cor_method)
@@ -79,7 +87,7 @@ cor_plot_wrapper <-
           y = y,
           x = x,
           type = "scatter",
-          y.plab = ylab,
+          y.plab = y_label,
           x.plab = label_attribute(data_set[[vars.ana[i]]]),
           mar = c(3.5, 3.5, 0.5, 0.5),
           mgp = c(2.0, 0.8, 0),
@@ -88,11 +96,38 @@ cor_plot_wrapper <-
         )
         xytline <- line(x = x, y = y)
         abline(coef(xytline))
-        legend("topleft",
-               paste0(cor_method, " r=", rho, ", ", p.txt),
-               cex = 1.0,
-               bty = "n")
+        legend(
+          "topleft",
+          paste0(cor_method, " r=", rho, ", ", p.txt),
+          cex = 1.0,
+          bty = "n"
+        )
         cat("\n")
       }
     }
+    
+  } else if (cor_format == "cor_table") {
+    x <- data_set |>
+      dplyr::select(all_of(var_focus)) |>
+      correlate(
+        use = "pairwise.complete.obs",
+        method = cor_method,
+        diagonal = 1,
+        quiet = T
+      ) |>
+      shave()
+    fashion(x) |> knitr::kable()
+  } else {
+    var_idx <- match(var_focus, names(data_set))
+    g <- ggpairs(
+      data = data_set,
+      columns = var_idx,
+      xlab = "Variables",
+      ylab = "Variables",
+      title = "Relationships between variables",
+      upper = list(continuous = wrap("cor", method = cor_method))
+    ) +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1))
+    return(g)
   }
+}
