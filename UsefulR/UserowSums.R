c260acda6a00d3a377ab412f1dc33550f9704ff7
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-02 08:25:09 -0400

    Create UserowSums.R

---
+++ UsefulR/UserowSums.R
@@ -0,0 +1,20 @@
+library(microbenchmark)
+rolls <- matrix(sample(1:9, 6, replace = TRUE), ncol = 2)
+
+app <- function(x) {
+    apply(x, 1, sum)
+}
+
+r_sum <- function(x) {
+    rowSums(x)
+}
+
+microbenchmark(
+    app_sol = app(rolls),
+    r_sum_sol = r_sum(rolls)
+)
+
+Unit: microseconds
+      expr   min      lq     mean median     uq      max neval cld
+   app_sol 14.30 14.9850 27.93317 15.695 16.615 1069.004   100   a
+ r_sum_sol  3.17  3.4245 15.91859  3.655  4.130 1172.435   100   a
