f442eae038e59700b9f90270993a821c969c1dea
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-02 08:22:03 -0400

    Update AvoidDataframe.R

--- UsefulR/AvoidDataframe.R
+++ UsefulR/AvoidDataframe.R
@@ -1,4 +1,5 @@
 library(microbenchmark)
+# Case 1
 microbenchmark(mat[, 1], df[, 1])
 
 Unit: microseconds
@@ -13,3 +14,28 @@ Unit: microseconds
      expr      min       lq       mean   median       uq      max neval cld
  mat[1, ]    4.520    5.130   12.17284   12.085   17.095   30.920   100  a 
   df[1, ] 4451.333 4528.663 4832.24897 4604.429 4723.460 7832.763   100   b
+
+# Case 2
+d <- function() {
+  data.frame(
+    d1 = sample(1:9, 3, replace = TRUE),
+    d2 = sample(1:9, 3, replace = TRUE)
+  )
+}
+
+m <- function() {
+  matrix(sample(1:9, 6, replace = TRUE), ncol = 2)
+}
+
+microbenchmark(
+ data.frame_solution = d(),
+ matrix_solution     = m()
+)
+
+Unit: microseconds
+                expr     min       lq     mean   median      uq       max neval
+ data.frame_solution 126.631 133.1305 165.7105 134.5555 141.451  2111.978   100
+     matrix_solution   4.920   5.6800 135.3040   7.9950   8.535 12738.269   100
+ cld
+   a
+   a
