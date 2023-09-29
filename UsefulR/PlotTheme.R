65ef4e42913da8e0a949e6fee2e9bde5419c1032
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-29 09:35:20 -0400

    Update Figure_Theme.R

--- UsefulR/PlotTheme.R
+++ UsefulR/PlotTheme.R
@@ -7,7 +7,8 @@ theme_BMA <- function(
       legend.position = "none",
       panel.background = element_rect(fill = "white", colour = "white"),
       axis.text.x = element_text(size = 9, angle = 90, hjust = 1, color = "black", vjust = 0.5),
-      strip.background = element_rect(fill = "white", color = "black", linewidth = 0.6), # Customize the box border in strip
+      strip.background = element_rect(fill = "white", color = "black", linewidth = 0.6), # Customize the box border in the strip
+      # strip.background = element_blank() # This is gonna remove the strip background
       strip.placement = "inside"
     )
 }
