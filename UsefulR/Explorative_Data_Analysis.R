7550fc07b5b49d206a161b87c2ba931de5073e88
Anni <90419301+anniliu7@users.noreply.github.com>
2023-10-02 07:34:09 -0400

    Update and rename EDA.R to Explorative_Data_Analysis.R

--- UsefulR/Explorative_Data_Analysis.R
+++ UsefulR/Explorative_Data_Analysis.R
@@ -1,3 +1,6 @@
 # Make a density plot
 plot(density(df$var), xlab="RNA velocity", main="Distribution of RNA velocity",
      lwd=3, col="chocolate", ylim=c(0, 0.08))
+
+# Make a histogram
+hist(df$var, breaks=50, xlab="RNA velocity", main="Distribution of RNA velocity")
