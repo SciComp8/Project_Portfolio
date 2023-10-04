d9b093aed2032a8f6474147e88103b33fc26e4c2
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-04 16:06:00 -0400

    Update Math_Function_v2.R

--- UsefulR/Math_Function_v2.R
+++ UsefulR/Math_Function_v2.R
@@ -3,3 +3,7 @@
 # e.g., sensitivity is the probability of a positive treatment response given that the patient has been treatment successfully.
 # specificity = true negative / (true negative + false positive)
 # e.g., specificity is the probability of a negative treatment response given that the patient has not been treatment successfully.
+
+
+# Global Sensitivity Analysis of Model Outputs
+https://cran.r-project.org/web/packages/sensitivity/index.html
