627e47b9b044f894fe574b3c8e768d273f2ae9df
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-04 16:02:13 -0400

    Create Math_Function_v2.R

---
+++ UsefulR/Math_Function_v2.R
@@ -0,0 +1,5 @@
+# Calculate the sensitivity/specificity
+# sensitivity = true positive / (true positive + false negative)
+# e.g., sensitivity is the probability of a positive treatment response given that the patient has been treatment successfully.
+# specificity = true negative / (true negative + false positive)
+# e.g., specificity is the probability of a negative treatment response given that the patient has not been treatment successfully.
