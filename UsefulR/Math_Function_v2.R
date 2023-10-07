6da2dbd0d8f0dac023557ef0c5d2b1d30714476c
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-07 11:54:38 -0400

    Update Math_Function_v2.R

--- UsefulR/Math_Function_v2.R
+++ UsefulR/Math_Function_v2.R
@@ -1,10 +1,15 @@
 # Calculate the sensitivity/specificity
 # sensitivity = no. of true positive / (no. of true positive + no. of false negative)
 ## e.g., sensitivity is the probability of a positive treatment response given that the patient has been treatment successfully.
+
 # specificity = no. of true negative / (no. of true negative + no. of false positive)
 ## e.g., specificity is the probability of a negative treatment response given that the patient has not been treatment successfully.
+
 # positive predictive value = (no. of true positives) / (no. of true positives + no. of false positives) 
 ## e.g., positive predictive value is the probability of being treatmented successfully given a positive treatment response.
+
+# negative predictive value = (no. of true negatives) / (no. of true negatives + no. of false negatives) 
+## e.g., negative predictive value is the probability of not being treatmented successfully given a negative treatment response.
 
 
 # Global Sensitivity Analysis of Model Outputs
