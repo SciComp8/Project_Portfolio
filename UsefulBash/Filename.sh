fea732e27e1b8bb30a5898d6758219f8e7bb87ef
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-23 09:19:02 -0400

    Update Filename.sh

--- UsefulBash/Filename.sh
+++ UsefulBash/Filename.sh
@@ -15,12 +15,17 @@ for f in $files; do grep ATAAC $f | tail -n 1; done
 files=relative_path/*.fastq
 for f in $files; do echo $f; head -n 2 $f | tail -n 1; done
 
+# Batch view the line numbers of all fastq files
+# inside the .sh
+wc -l $@ | grep -v total
+
+wc -l $@ | grep -v total | sort -n
+
 # Extract the base filenames (without the .tsv.gz extension) of the input files specified by the first and second arguments 
 r1=$(basename $1 .tsv.gz)
 r2=$(basename $2 .tsv.gz)
 echo $r1 $r2
 # bash Filename.sh barcodes1.tsv.gz barcodes2.tsv.gz
-
 
 # Remove files with specific shared base filenames
 # More info: https://unix.stackexchange.com/questions/306940/what-is-the-purpose-of-the-do-keyword-in-bash-for-loops
@@ -37,7 +42,6 @@ echo $filename | awk -F"." 'BEGIN{OFS="_"} {print $1,$2}'
 echo $filename | awk -F"." -v OFS="_" '{print $1,$2}'
 # Output: SRR8990876_1_paired
 
-
 # Output variables into a file with the specific filename
 filename=$(echo $(basename barcodes1 .tsv.gz))
 for i in `echo $var1`
@@ -45,12 +49,10 @@ do
   echo $var1 $var2 $var3 $i >>$filename.txt 
 done
 
-
 # Compress/decompress a file with the specific filename
 filename=$(echo $(basename SRR8990876_1.paired .fastq.gz))
 gzip $filename.fastq
 gunzip $filename.fastq.gz
-
 
 # Create a backup copy of the file with the specified filename
 fastq=$1 # Store the first argument passed to the program as variable fastq
