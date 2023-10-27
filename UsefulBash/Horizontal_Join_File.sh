e5baffe0bef3dbc7cf7f173696843ae28e97e3d2
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-27 16:35:43 -0700

    Update Horizontal_Join_File.sh

--- UsefulBash/Horizontal_Join_File.sh
+++ UsefulBash/Horizontal_Join_File.sh
@@ -1,1 +1,5 @@
 paste -d "|" file_name_1 file_name_2 file_name_3
+
+paste -d "|," file_name_1 file_name_2 file_name_3
+# The contents between the 1st and 2nd files are separated by |
+# The contents between the 2nd and 3rd files are separated by ,
