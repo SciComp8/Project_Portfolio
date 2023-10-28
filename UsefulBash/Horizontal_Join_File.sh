ceff6eeedbc4203c5bbb4db2b98e6b8efc50d46a
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-27 19:25:10 -0700

    Update Horizontal_Join_File.sh

--- UsefulBash/Horizontal_Join_File.sh
+++ UsefulBash/Horizontal_Join_File.sh
@@ -5,5 +5,6 @@ paste -d "|," file_name_1 file_name_2 file_name_3
 # The contents between the 2nd and 3rd files are separated by ,
 
 paste -s -d ":" file_name_1 file_name_2 file_name_3
+# -s: read all the lines from a single file and merge all these lines into a single line with each line separated by tab; these single lines are separated by newline
 
 cut -d " " -f 1 file_name_2 | paste file_name_1 -
