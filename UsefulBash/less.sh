7e11a4bf7001546c66c6eeb8f7100196347a258d
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-30 18:48:43 -0400

    Create less.sh

---
+++ UsefulBash/less.sh
@@ -0,0 +1,5 @@
+# Press spacebar to page down
+# Type :n to go to the second file
+# Type :p to go back to the previous file
+# Type :q to quit
+less relative_path/file_name_1.text relative_path/file_name_2.text
