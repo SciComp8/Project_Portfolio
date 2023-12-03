d16bc5ab1da247fccdb2927fae6a098ebfd0d122
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-02 20:25:49 -0500

    Update and rename slice_max.R to slice_function.R

--- UsefulR/slice_max.R
+++
@@ -1,6 +0,0 @@
-# Give a dataframe with 3 columns - seed, class, frequency where each seed has multiple classes and each class has its own frequency
-# Extract the top 10 most frequent class per seed
-BMI.class.freq.top10.per.seed <- BMI.class.freq |> 
-  group_by(Seed) |> 
-  slice_max(order_by = Frequency, n = 10, with_ties = F) |>
-  ungroup()
