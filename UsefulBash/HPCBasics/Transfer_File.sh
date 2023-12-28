a0213009bfcddcdb7914521525bb4363fbbb5876
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-12-28 17:54:17 -0500

    Create Transfer_File.sh

---
+++ UsefulBash/HPCBasics/Transfer_File.sh
@@ -0,0 +1,8 @@
+# Open the local terminal
+scp ~/Desktop/scRNA_seq.slurm your_name@pascal.med.cornell.edu:/home/your_name/project
+
+# Open the HPC environment
+ssh pascal
+ssh curie.pbtech
+cd project
+ls
