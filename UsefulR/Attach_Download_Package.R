f334676434c401fdeeec3e1534f7cb6a47bb8611
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-01-04 15:06:25 -0500

    Create Attach_Download_Package.R

---
+++ UsefulR/Attach_Download_Package.R
@@ -0,0 +1,18 @@
+bio_pkgs <- c("GenomicRanges", "Rsamtools", "rtracklayer", "GenomicAlignments", "ShortRead")
+gen_pkgs <- c("ggplot2")
+for (pkg in c(bio_pkgs, gen_pkgs)) {
+  if (!require(pkg, character.only = T) & (pkg %in% bio_pkgs)) {
+    BiocManager::install(pkg)
+    library(pkg, character.only = T)
+  } else if (!require(pkg, character.only = T) & (pkg %in% gen_pkgs)) {
+    install.packages(pkg)
+    library(pkg, character.only = T)
+  } else {
+    library(pkg, character.only = T)
+  }
+}
+
+a = "GenomicRanges"
+library(a)
+# Error in library(a) : there is no package called ‘a’
+library(a, character.only = T)
