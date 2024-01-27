5a7be433824c06e7eeb79e649f5c25d43e1406b7
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-01-26 22:49:56 -0500

    Update Report_Machine_Specs.R

--- UsefulR/Report_Machine_Specs.R
+++ UsefulR/Report_Machine_Specs.R
@@ -6,3 +6,8 @@ get_ram()
 get_platform_info()$OS.type
 get_r_version()$platform 
 get_r_version()$version.string 
+
+# Print the machine specifications in a file
+sink("2024Jan09_machinespecs.txt")
+get_cpu()
+sink()
