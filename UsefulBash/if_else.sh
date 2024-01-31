be0ed0be511db10a1214dc97457483c9a1836175
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-01-30 20:56:10 -0500

    Create if_else.sh

---
+++ UsefulBash/if_else.sh
@@ -0,0 +1,8 @@
+prime=5
+prime_direction=""
+if [[ $prime == 3 ]]; then
+    prime_direction="--3prime "
+    echo "The prime direction is $prime_direction"
+else
+    echo "The prime direction is 5prime(default)"
+fi
