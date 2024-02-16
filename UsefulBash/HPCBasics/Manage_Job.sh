b592b16a732390566c4515b83b7e3c84c8edbc82
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-02-16 16:54:45 -0500

    Update Manage_Job.sh

--- UsefulBash/HPCBasics/Manage_Job.sh
+++ UsefulBash/HPCBasics/Manage_Job.sh
@@ -11,8 +11,14 @@ squeue -u username -p=panda
 # Show the info of past jobs
 sacct 
 sacct -j 11114265
+# JobID    JobName  Partition    Account  AllocCPUS      State ExitCode
 sacct -r panda
 scontrol show job 53362163
+# JobId | JobName | UserId | GroupId | MCS_label | Priority | Nice | Account | QOS | JobState | Reason | Dependency | Requeue 
+# Restarts | BatchFlag | Reboot | ExitCode | RunTime | TimeLimit | TimeMin | SubmitTime | EligibleTime | StartTime | EndTime | Deadline
+# PreemptTime | SuspendTime | SecsPreSuspend | Partition | AllocNode:Sid | ReqNodeList | ExcNodeList | NodeList | NumNodes | NumCPUs | NumTasks
+# CPUs/Task | ReqB:S:C:T | TRES | node | Socks/Node | NtasksPerN:B:S:C | CoreSpec | MinCPUsNode | MinMemoryNode | MinTmpDiskNode | Features
+# DelayBoot | Gres | Reservation | OverSubscribe | Contiguous | Licenses | Network | Command | WorkDir | StdErr | StdIn | StdOut | Power
 
 # Cancel/pause/resume jobs
 scancel 11114268 # !
