095c62c8902468f3a0642dac844700f6bf85a48d
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-02-27 21:44:41 -0500

    Update and rename Use_rowSums.R to Row_Operation.R

--- UsefulR/Row_Operation.R
+++ UsefulR/Row_Operation.R
@@ -1,3 +1,7 @@
+# Move the first row to the last row
+df_fix <- rbind(df[-1, ], df[1, ]) 
+
+# Summation per row
 library(microbenchmark)
 rolls <- matrix(sample(1:9, 6, replace = TRUE), ncol = 2)
 
@@ -19,6 +23,5 @@ Unit: microseconds
    app_sol 14.30 14.9850 27.93317 15.695 16.615 1069.004   100   a
  r_sum_sol  3.17  3.4245 15.91859  3.655  4.130 1172.435   100   a
 
-# DNA methylation analysis scenarior:
-# Remove any CpG probes that have any samples showing detection p value >= 0.01
+# Think of DNA methylation analysis scenarior: remove any CpG probes that have any samples showing detection p value >= 0.01
 probe.keep <- rowSums(det.p.filter < 0.01) == ncol(norm.set) 
