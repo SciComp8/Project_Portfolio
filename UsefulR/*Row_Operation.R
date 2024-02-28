142234e7e955e017893cd892e0b9102c660c9fe6
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-02-27 21:45:29 -0500

    Update and rename Row_Operation.R to *Row_Operation.R

--- UsefulR/*Row_Operation.R
+++ UsefulR/*Row_Operation.R
@@ -25,3 +25,4 @@ Unit: microseconds
 
 # Think of DNA methylation analysis scenarior: remove any CpG probes that have any samples showing detection p value >= 0.01
 probe.keep <- rowSums(det.p.filter < 0.01) == ncol(norm.set) 
+
