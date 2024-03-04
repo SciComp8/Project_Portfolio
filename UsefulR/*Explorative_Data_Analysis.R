eb051e7d4b921d1eb13dfd61a67829e549dac6d5
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-03-04 16:52:26 -0500

    Update *Explorative_Data_Analysis.R

--- UsefulR/*Explorative_Data_Analysis.R
+++ UsefulR/*Explorative_Data_Analysis.R
@@ -5,3 +5,11 @@ plot(density(df$var), xlab="RNA velocity", main="Distribution of RNA velocity",
 # Make a histogram
 hist(df$var, breaks=50, xlab="RNA velocity", main="Distribution of RNA velocity")
 hist(peak_data$peak_ratio, breaks=50, xlab="Peak ratio (contaminated peak / normal peak)", main="Distribution of peak ratio")
+hist_data <- hist(peak_data$peak_ratio, plot = FALSE) # Generate histogram data without plotting
+hist_data$counts <- log2(hist_data$counts+1) # Transform frequencies to log2 scale; +1 to avoid log(0); 
+# Plot the histogram with log2-transformed frequencies
+plot(hist_data, freq = TRUE, breaks=50, 
+     main = "Distribution of peak ratio with log2 transformed frequencies",
+     ylab = "log2 (rrequency)",
+     xlab = "Peak ratio",
+     ylim = c(0, max(hist_data$counts))) # Adjust ylim based on transformed frequencies
