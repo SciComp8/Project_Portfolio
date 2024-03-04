1876985fdda9d1527d0ac27449e828b4f4285b9e
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-03-04 16:46:30 -0500

    Update and rename Explorative_Data_Analysis.R to *Explorative_Data_Analysis.R

--- UsefulR/*Explorative_Data_Analysis.R
+++ UsefulR/*Explorative_Data_Analysis.R
@@ -4,3 +4,4 @@ plot(density(df$var), xlab="RNA velocity", main="Distribution of RNA velocity",
 
 # Make a histogram
 hist(df$var, breaks=50, xlab="RNA velocity", main="Distribution of RNA velocity")
+hist(peak_data$peak_ratio, breaks=50, xlab="Peak ratio (contaminated peak / normal peak)", main="Distribution of peak ratio")
