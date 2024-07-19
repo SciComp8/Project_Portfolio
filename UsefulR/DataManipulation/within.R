4d2f6836be00539af493e51dcf29c70252573f03
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-07-18 22:03:12 -0400

    Update within.R

--- UsefulR/DataManipulation/within.R
+++ UsefulR/DataManipulation/within.R
@@ -5,3 +5,7 @@ seurat_obj_2 <- within(seurat_obj@meta.data, {
   new_metric <- nCount_RNA / percent.mt  # add a new column 
   rm(orig.ident, nFeature_RNA)  # delete unnecessary columns
 })
+
+# within() examines the environment after the evaluation of expr 
+# and makes the corresponding modifications to a copy of data (this may fail in the data frame case if objects are created which cannot be stored in a data frame), and returns it. 
+# within() can be used as an alternative to transform.
