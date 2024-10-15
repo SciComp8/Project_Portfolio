c80e5ae56845e14022aaa834d529952af7be497b
LAN...COME <90419301+ScienceComputing@users.noreply.github.com>
2024-10-15 15:14:32 -0400

    Update *Explorative_Data_Analysis.R

--- UsefulR/*Explorative_Data_Analysis.R
+++ UsefulR/*Explorative_Data_Analysis.R
@@ -2,6 +2,22 @@
 plot(density(df$var), xlab="RNA velocity", main="Distribution of RNA velocity",
      lwd=3, col="chocolate", ylim=c(0, 0.08))
 
+dat0 <- read_xls(
+  path = "../Data/Raw/41586_2012_BFnature11735_MOESM103_ESM.xls", 
+  sheet = "Table S11",
+  col_names = T,
+  na = c("Unknown", "unknown", "N/A", "n/a"),
+  skip = 1
+) 
+dat0 <- dat0[, -13]
+dat0 <- dat0 |>
+  clean_names() 
+dim(dat0) 
+
+summary(dat0$act_area_raw)
+plot(density(dat0$act_area_raw), xlab="Raw Activity Area", main="Distribution of Raw Activity Area",
+     lwd=3, col="chocolate")
+
 # Make a histogram
 hist(df$var, breaks=50, xlab="RNA velocity", main="Distribution of RNA velocity")
 hist(peak_data$peak_ratio, breaks=50, xlab="Peak ratio (contaminated peak / normal peak)", main="Distribution of peak ratio")
